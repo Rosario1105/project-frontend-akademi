5a01042532a0cca896f7333236fa3e3117e8052e
Rosario Pittaro <rosario.pittaro1105@gmail.com>
2025-04-07 13:00:58 -0300

    Feat: Scroll solucinado y proyecto listo

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,10 +4,12 @@ import App from "../App";
 import NewProduct from "../pages/NewProduct";
 import ProductDetail from "../pages/ProductDetail";
 import ProductList from "../pages/ProductList";
+import Scroll from "../components/product/Scroll";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Scroll/>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/add-product" element={<NewProduct />} />
